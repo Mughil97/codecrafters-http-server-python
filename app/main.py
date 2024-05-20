0a27481f54868a7b22e62996770b8532199d4fca
Mughil97 <mmughil97@gmail.com>
2024-05-20 22:32:57 +0530

    test3

--- app/main.py
+++ app/main.py
@@ -8,9 +8,13 @@ def main():
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     conn, _ = server_socket.accept()  # wait for client
+    data = conn.recv(1024).decode("utf-8")
     with conn:
-        conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
-        conn.close()
+        if data.split(" ")[1] == "/":
+            conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+            conn.close()
+        else:
+            conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
 
 if __name__ == "__main__":
