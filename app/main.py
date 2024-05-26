660588d65f8c6b8182a53bb0a3315c6cf4c14bf7
Mughil97 <mmughil97@gmail.com>
2024-05-26 20:14:50 +0530

    test4

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 # Uncomment this to pass the first stage
+import re
 import socket
+
+compile = re.compile(r"\/echo\/([a-z0-9])+")
 
 
 def main():
@@ -10,11 +13,31 @@ def main():
     conn, _ = server_socket.accept()  # wait for client
     data = conn.recv(1024).decode("utf-8")
     with conn:
-        if data.split(" ")[1] == "/":
+        message_parts = extract_http_message(data)
+
+        if message_parts["url"] == "/":
             conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+            conn.close()
+        if compile.match(message_parts["url"]):
+            echo_message = message_parts["url"].split("/")[2]
+            headers = f"Content-Type: text/plain\r\nContent-Length: {len(echo_message)}"
+            response = f"HTTP/1.1 200 OK\r\n{headers}\r\n\r\n{echo_message}"
+            conn.sendall(response.encode("utf-8"))
             conn.close()
         else:
             conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+
+
+def extract_http_message(http_packet) -> dict:
+    http_lines = http_packet.split("\r\n")
+    start_line_items = http_lines[0].split(" ")
+    return {
+        "verb": start_line_items[0],
+        "url": start_line_items[1],
+        "version": start_line_items[2],
+        "headers": http_lines[1],
+        "body": http_lines[3],
+    }
 
 
 if __name__ == "__main__":
