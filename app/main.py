8523c7576f63337ec96aaed94a53dbc06a88c061
Mughil97 <mmughil97@gmail.com>
2024-10-16 15:33:51 +0530

    can read file

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ import socket
 import threading
 
 compile = re.compile(r"\/echo\/([a-z0-9])+")
+file_compile = re.compile(r"\/files\/([a-z0-9])+")
 
 
 def main():
@@ -42,6 +43,18 @@ def handle_client(conn) -> None:
             response = f"HTTP/1.1 200 OK\r\n{headers}\r\n\r\n{message}"
             conn.sendall(response.encode("utf-8"))
             conn.close()
+        if file_compile.match(message_parts["url"]):
+            try:
+                file_content = open(
+                    f"/tmp/data/codecrafters.io/http-server-tester/{message_parts["url"].split("/")[2]}",
+                    "rb",
+                ).read()
+                headers = f"Content-Type: application/octet-stream\r\nContent-Length: {len(file_content)}"
+                response = f"HTTP/1.1 200 OK\r\n{headers}\r\n\r\n{file_content.decode("utf-8")}"
+                conn.sendall(response.encode("utf-8"))
+                conn.close()
+            except FileNotFoundError:
+                conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
         else:
             conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
